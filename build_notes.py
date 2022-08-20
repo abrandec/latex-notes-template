b4c5fb8d4261cb086ba5b7b3537b6f9c89a6a335
e <abrandecarlo@protonmail.com>
2022-08-19 19:57:30 -0700

    preamble now generated via python

--- build_notes.py
+++ build_notes.py
@@ -1,7 +1,32 @@
 #!/usr/bin/env python3
-from distutils.command.build import build
 import subprocess
-import os, sys, getopt
+import os, sys, getopt, re
+
+def gen_preamble(note) -> None:
+    """
+    Generate the preamble for the notes.
+    :param note: The note to generate the preamble for.
+    :param note: str
+    :return: None
+    """
+    author = ''
+    author_latex = '\n\\author '
+    if os.path.exists('.cfg'):
+        with open('.cfg', 'r') as f:
+            for line in f:
+                author = re.search('(?<=Author: ).*(?=)', line).group(0)
+                if author != '':
+                    author_latex = author_latex + '{' + author + '}' + '\n'
+                    break
+    else:
+        print("No .cfg file found.")
+        sys.exit()
+    note_dir = os.path.join('./src', note)
+    if os.path.exists(f"{note_dir}/preamble.tex"):
+        subprocess.call(f"rm {note_dir}/preamble.tex", shell=True)
+    subprocess.call(f"cp lib/preamble.tex {note_dir}/preamble.tex", shell=True)
+    with open(f"{note_dir}/preamble.tex", 'a') as f:
+        f.write(f"{author_latex}")
 
 def __build_notes(note) -> None:
     """
@@ -12,16 +37,16 @@ def __build_notes(note) -> None:
     """
     note_dir = os.path.join('./src', note)
     # Build the note.
-    subprocess.call('pdflatex master.tex', shell=True, cwd=note_dir) 
+    subprocess.call('pdflatex main.tex', shell=True, cwd=note_dir) 
     # Move the pdf to the build directory
-    if os.path.exists(os.path.join(note_dir, 'master.pdf')):     
+    if os.path.exists(os.path.join(note_dir, 'main.pdf')):     
         if not os.path.exists(f'./build/{note}'):
             os.makedirs(f'./build/{note}')
         if os.path.exists(f'./build/{note}.pdf'):
            subprocess.call(f'rm ./build/{note}.pdf', shell=True)
-        subprocess.call(f'mv master.pdf ../../build/{note}', shell=True, cwd=note_dir)
+        subprocess.call(f'mv main.pdf ../../build/{note}', shell=True, cwd=note_dir)
         # Rename the pdf to the note name.
-        subprocess.call(f'mv build/{note}/master.pdf build/{note}/{note}.pdf', shell=True)
+        subprocess.call(f'mv build/{note}/main.pdf build/{note}/{note}.pdf', shell=True)
     
 def build_notes(note) -> None:
     """
@@ -30,6 +55,7 @@ def build_notes(note) -> None:
     :param notes: list
     :return: None
     """
+    gen_preamble(note)
     __build_notes(note)
     __build_notes(note)
 
@@ -42,6 +68,7 @@ def clean_src(note) -> None:
     """
     note_dir = os.path.join('./src', note)
     subprocess.call(f'rm -f *.dvi *.ptc *.log *.aux *.toc *.out', shell=True, cwd=note_dir)
+    subprocess.call(f'rm preamble.tex', shell=True, cwd=note_dir)
 
 def main(argv) -> None:
     """
@@ -78,7 +105,7 @@ def main(argv) -> None:
     elif note != '':
         build_notes(note)
         clean_src(note)
-        print("Notes built.")
+    print("Notes built.")
 
 if __name__ == "__main__":
     main(sys.argv[1:])
