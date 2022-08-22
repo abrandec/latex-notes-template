fa41161e69535ccbb73abdb4ecbd3358df4c2a2d
e <abrandecarlo@protonmail.com>
2022-08-21 18:46:06 -0700

    cleanup

--- build_notes.py
+++ build_notes.py
@@ -49,114 +49,6 @@ class Note_Generator:
             file = re.sub('(?:makeatother)', title_latex, file)
         with open(f"{self.note_dir}/main_temp.tex", 'w') as f:
             f.write(file)
-    
-    def __get_definitions(self, file: str, lemma: str) -> list[str]:
-        """
-        Get the definitions from the latex file.
-        :param file: The latex file to get the definitions from.
-        :param file: str
-        :return: list[str]
-        """
-        with open(file, 'r') as f:
-            file = f.read()
-            matches = None
-            try:
-                lemma_re = r'(?s)(?<=\\begin{' + lemma + r'}).*?(?=\\end{' + lemma + r'})'
-                matches = re.findall(lemma_re, file)
-            except Exception:
-                print("No definitions for this chapter found.")
-            return matches
-
-    def __parse_definitions(self, matches, matches_comp: list) -> None:
-        """
-        Get the hyperlink section of the definition.
-        :param matches: Defintions to parse.
-        :param raw_def: str
-        :param matches_comp: List of matches to append to.
-        :param matches_comp: list[str]
-        """
-        for match in matches:
-            # Debug these regexes yourself.
-            def_hyperref = re.search(r'(?s)(?<=\\label{).*?(?=})', match).group(0)
-            def_name_re = r'(?s)(?<=' + def_hyperref + r'}' + r'\n).*?(?=\\newline)'
-            def_name = re.search(def_name_re, match).group(0)
-            def_name = def_name.strip()
-            def_text_re = r'(?s)(?<=' + def_name + r' \\newline\n).*$'
-            def_text = re.search(def_text_re, match).group(0)
-            matches_comp.append([match, def_hyperref, def_name, def_text])
-            # Sort definitions by alphabetical order.
-            matches_comp.sort(key=lambda x: x[2])
-
-    def __craft_definitions_page(self, matches_comp: list, lemma: str) -> None:
-        """
-        Craft the definitions page with all the definitions and sections/subsections.
-        :param matches_comp: List of matches to append to.
-        :param matches_comp: list[str]
-        :return: None
-        """
-        with open(f"{self.note_dir}/{lemma}s.tex", 'r') as f:
-            file = f.read()
-        lemma_cap = lemma[0].upper() + lemma[1:] + 's'
-        file = "\setcounter{chapter}{" + str(self.chapter_counter - 1) + "}\n\chapter{" + lemma_cap + "}\n"
-        self.chapter_counter += 1
-        sections = []
-        section_template = '\section{'
-        definition_template_0 = '\n\subsection{'
-        definition_template_1 = '}\hyperref['
-        definition_template_2 = ']{link}\n\n'
-        # Generate sections alphabetically.
-        for match in matches_comp:
-            section = section_template + match[2][0] + '}\n'
-            definition = definition_template_0 + match[2] + definition_template_1 + match[1] + definition_template_2 + match[2]
-            if section not in sections:
-                sections.append(section)
-                file = file + sections[-1] + definition
-            else:
-                file = file.replace(section, section + definition)
-        with open(f"{self.note_dir}/{lemma}s.tex", 'w') as f:
-            f.write(file)
-
-    def __gen_definitions_page(self, lemma: str) -> None:
-        """
-        Extract and generate a page with all definitions.
-        :return: None
-        """
-        # [match_str, hyperref, definition_name, definition_text]
-        matches_comp = []
-        # Extract the definitions.
-        for f in os.listdir(self.note_dir):
-            if re.search(r'(?<=chpt_).*(?=.tex)', f):    
-                matches = self.__get_definitions(f'{self.note_dir}/{f}', lemma)
-                print(matches)
-                self.__parse_definitions(matches, matches_comp)
-        # Create the definitions.tex file.
-        if os.path.exists(f"{self.note_dir}/{lemma}s.tex"):
-            subprocess.call(f"rm {self.note_dir}/{lemma}s.tex", shell=True)
-        subprocess.call(f"touch {self.note_dir}/{lemma}s.tex", shell=True)
-        self.__craft_definitions_page(matches_comp, lemma)
-
-    def __gen_theorem_definition_pages(self) -> None:
-            """
-            Scan main file for theorem imports.
-            :return: None
-            """
-            with open(f"{self.note_dir}/main_temp.tex", 'r') as f:
-                main_file = f.read()
-            with open('lib/preamble.tex', 'r') as f:
-                preamble_file = f.read()
-            f.close()
-            matches_l1 = re.findall(r'(?s)(?<=\\declaretheorem).*?(?=\\)', preamble_file)
-            temp: str = ''
-            for match in matches_l1:
-                temp += match + '\n'
-            declared_theorems = re.findall(r'(?s)(?<={).*?(?=})', temp)
-            for theorem in declared_theorems:
-                theorem_input = r'\\input{' + theorem + 's.tex}' 
-                try:
-                    re.search(theorem_input, main_file).group(0)
-                    self.__gen_definitions_page(theorem)
-                except Exception:
-                    pass
             
     def __update_chapter_numbers(self) -> None:
         """
@@ -179,6 +71,120 @@ class Note_Generator:
             except Exception:
                 continue
 
+    # Theorem pages generation zone.
+    def __get_theorems(self, file: str, theorem: str) -> list[str]:
+        """
+        Get the theorems from the latex file.
+        :param file: The latex file to get the theorems from.
+        :param file: str
+        :param theorem: The theorem to get the theorems for.
+        :param theorem: str
+        :return: list[str]
+        """
+        with open(file, 'r') as f:
+            file = f.read()
+            matches = None
+            try:
+                theorem_re = r'(?s)(?<=\\begin{' + theorem + r'}).*?(?=\\end{' + theorem + r'})'
+                matches = re.findall(theorem_re, file)
+            except Exception:
+                print("No definitions for this chapter found.")
+            return matches
+
+    def __parse_theorems(self, matches, matches_comp: list) -> None:
+        """
+        Get the hyperlink section of the theorem.
+        :param matches: Defintions to parse.
+        :param raw_def: str
+        :param matches_comp: List of matches to append to.
+        :param matches_comp: list[str]
+        """
+        for match in matches:
+            # Debug these regexes yourself.
+            def_hyperref = re.search(r'(?s)(?<=\\label{).*?(?=})', match).group(0)
+            def_name_re = r'(?s)(?<=' + def_hyperref + r'}' + r'\n).*?(?=\\newline)'
+            def_name = re.search(def_name_re, match).group(0)
+            def_name = def_name.strip()
+            def_text_re = r'(?s)(?<=' + def_name + r' \\newline\n).*$'
+            def_text = re.search(def_text_re, match).group(0)
+            matches_comp.append([match, def_hyperref, def_name, def_text])
+            # Sort definitions by alphabetical order.
+            matches_comp.sort(key=lambda x: x[2])
+
+    def __craft_theorem_page(self, matches_comp: list, theorem: str) -> None:
+        """
+        Craft the theorem page with all the theorems and sections/subsections.
+        :param matches_comp: List of matches to append to.
+        :param matches_comp: list[str]
+        :param theorem: The theorem to craft the theorem page for.
+        :param theorem: str
+        :return: None
+        """
+        with open(f"{self.note_dir}/{theorem}s.tex", 'r') as f:
+            file = f.read()
+        theorem_cap = theorem[0].upper() + theorem[1:] + 's'
+        file = "\setcounter{chapter}{" + str(self.chapter_counter - 1) + "}\n\chapter{" + theorem_cap + "}\n"
+        self.chapter_counter += 1
+        sections = []
+        section_template = '\section{'
+        theorem_template_0 = '\n\subsection{'
+        theorem_template_1 = '}\hyperref['
+        theorem_template_2 = ']{link}\n\n'
+        # Generate sections alphabetically.
+        for match in matches_comp:
+            section = section_template + match[2][0] + '}\n'
+            full_theorem = theorem_template_0 + match[2] + theorem_template_1 + match[1] + theorem_template_2 + match[2]
+            if section not in sections:
+                sections.append(section)
+                file = file + sections[-1] + full_theorem
+            else:
+                file = file.replace(section, section + full_theorem)
+        with open(f"{self.note_dir}/{theorem}s.tex", 'w') as f:
+            f.write(file)
+
+    def __fill_theorem_page(self, theorem: str) -> None:
+        """
+        Extract and generate a page with all theorems.
+        :param theorem: The theorem to generate the theorems page for.
+        :param theorem: str
+        :return: None
+        """
+        # [match_str, hyperref, theorem_name, theorem_text]
+        matches_comp = []
+        # Extract the theorems.
+        for f in os.listdir(self.note_dir):
+            if re.search(r'(?<=chpt_).*(?=.tex)', f):    
+                matches = self.__get_theorems(f'{self.note_dir}/{f}', theorem)
+                self.__parse_theorems(matches, matches_comp)
+        # Create the {theorem}s.tex file.
+        if os.path.exists(f"{self.note_dir}/{theorem}s.tex"):
+            subprocess.call(f"rm {self.note_dir}/{theorem}s.tex", shell=True)
+        subprocess.call(f"touch {self.note_dir}/{theorem}s.tex", shell=True)
+        self.__craft_theorem_page(matches_comp, theorem)
+
+    def __gen_theorems_pages(self) -> None:
+            """
+            Generate all theorem pages.
+            :return: None
+            """
+            with open(f"{self.note_dir}/main_temp.tex", 'r') as f:
+                main_file = f.read()
+            with open('lib/preamble.tex', 'r') as f:
+                preamble_file = f.read()
+            f.close()
+            matches_l1 = re.findall(r'(?s)(?<=\\declaretheorem).*?(?=\\)', preamble_file)
+            temp: str = ''
+            for match in matches_l1:
+                temp += match + '\n'
+            declared_theorems = re.findall(r'(?s)(?<={).*?(?=})', temp)
+            for theorem in declared_theorems:
+                theorem_input = r'\\input{' + theorem + 's.tex}' 
+                try:
+                    re.search(theorem_input, main_file).group(0)
+                    self.__fill_theorem_page(theorem)
+                except Exception:
+                    pass
+    
     def __build_notes(self) -> None:
         """
         Build the note.
@@ -216,7 +222,7 @@ class Note_Generator:
         self.note_dir = os.path.join('./src', note)
         self.__gen_preamble()
         self.__update_chapter_numbers()
-        self.__gen_theorem_definition_pages()
+        self.__gen_theorems_pages()
         # Call __build_notes() twice since Table of Contents isn't added properly the first time with (funny business with pdflatex).
         self.__build_notes()
         self.__build_notes()
